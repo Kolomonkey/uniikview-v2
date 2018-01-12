8723e7a374765f9df54e26f7271a552e30a31dea
Josh de Gouveia <josh@unied.com>
2018-01-12 16:06:00 -0700

    Setup Application. Created Routing Module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
